d0fb73716a09df4ca1d6f7f377bfd0a8865cb221
RZcodeCloud <133132296+RZcodeCloud@users.noreply.github.com>
2023-10-16 19:47:03 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+    // Reload the webflow js script 0.2 seconds after the page has finished loading
+	document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        Webflow.require("ix2").init();
+    }, 200);
+});
